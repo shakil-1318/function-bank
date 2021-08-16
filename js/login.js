eacfade421e0a8f525b466866f787f0952484f42
shakilhossain-gite <mdshakilh354@gmail.comm>
2021-08-16 17:59:52 +0600

    login func add

---
+++ js/login.js
@@ -0,0 +1,20 @@
+document.getElementById('login-submit').addEventListener('click', function(){
+
+    function getUserInformation(information){
+        const userInformation = document.getElementById(information);
+        const userField = userInformation.value;
+        userInformation.value = '';
+        return userField;
+        
+    }
+    const userField = getUserInformation('user-email');
+
+    const passwordField = getUserInformation('user-password')
+    
+    if(userField == 'baper@baank.com' && passwordField == 'secret'){
+        window.location.href = 'banking.html';
+    }
+    else{
+        alert('sorry, your informtion wrong');
+    }
+});
